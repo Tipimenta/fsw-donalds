86a03742728af94157e3ab19743af8dae339d839
tipimenta <tiagopimenta.ata@gmail.com>
2025-02-22 11:41:24 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
   return (
-<Button>Teste</Button>
-  )
-}
+    <Button className="rounded-sm border border-red-500 p-5">Teste</Button>
+  );
+};
 export default HomePage;
