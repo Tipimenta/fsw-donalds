feb36214e5082090c27a7d6961491181ffdbd5dd
tipimenta <tiagopimenta.ata@gmail.com>
2025-03-01 19:21:00 -0300

    feat: add initial card context and card sheet component

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -1,102 +1,119 @@
 "use client";
 
-import {Prisma} from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import {  useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
 
+import CardSheet from "../../components/card-sheet";
+import { CardContext } from "../../contexts/card";
+
+
+
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
-    include: {restaurant: {select: {avatarImageUrl: true; name: true;}}};
+    include: { restaurant: { select: { avatarImageUrl: true; name: true; } } };
   }>;
 }
 
-const ProductDetails = ({product} : ProductDetailsProps) => {
- const [quantity, setQuantity] = useState<number>(1);
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const { toggleCard} = useContext(CardContext)
+  const [quantity, setQuantity] = useState<number>(1);
 
- const handleDecreaseQuantity = () => {
-     setQuantity((prev) => {
-         if (prev === 1) {
-             return 1;
-   } 
-   return prev - 1;
-  } )}
+  const handleDecreaseQuantity = () => {
+    setQuantity((prev) => {
+      if (prev === 1) {
+        return 1;
+      }
+      return prev - 1;
+    })
+  }
 
- const handleIncreaseQuantity = () => {
+  const handleIncreaseQuantity = () => {
     setQuantity((prev) => prev + 1);
+  }
 
-}
- return (
-  <div className="relative z-50 rounded-t-3xl p-5 mt-[1.5rem] flex flex-auto flex-col overflow-hidden"  >
+  const handleAddToCard = () => {
+    toggleCard();
+  }
 
-   <div className="flex-auto overflow-hidden">
-     {/* RESTAURANTE */}
-     <div className="flex items-center gap-1.5">
+
+  return (
+    <>
+<div className="relative z-50 rounded-t-3xl p-5 mt-[1.5rem] flex flex-auto flex-col overflow-hidden"  >
+
+<div className="flex-auto overflow-hidden">
+  {/* RESTAURANTE */}
+  <div className="flex items-center gap-1.5">
     <Image
-    src={product.restaurant.avatarImageUrl}
-    alt={product.restaurant.name}
-    width={16}
-    height={16}
-    className="rounded-full"
+      src={product.restaurant.avatarImageUrl}
+      alt={product.restaurant.name}
+      width={16}
+      height={16}
+      className="rounded-full"
     />
     <p className="text-xs text-muted-foreground ">{product.restaurant.name}</p>
+  </div>
+
+  {/* NOME DO PRODUTO */}
+  <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+
+  {/* PREÇO E QUANTIDADE */}
+  <div className="flex items-center justify-between mt-3">
+    <h3 className="text-xl font-semibold">
+      {formatCurrency(product.price)}
+    </h3>
+    <div className="flex items-center gap-3 text-center">
+      <Button
+        variant="outline"
+        className="w-8 h-8 rounded-xl"
+        onClick={handleDecreaseQuantity}
+      ><ChevronLeftIcon /></Button>
+      <p className="w-3">{quantity}</p>
+      <Button
+        variant="destructive"
+        className="w-8 h-8 rounded-xl"
+        onClick={handleIncreaseQuantity}
+      ><ChevronRightIcon /></Button>
     </div>
+  </div>
 
-    {/* NOME DO PRODUTO */}
-    <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
-
-    {/* PREÇO E QUANTIDADE */}
-    <div className="flex items-center justify-between mt-3">
-       <h3 className="text-xl font-semibold">
-       {formatCurrency(product.price)}
-        </h3>  
-        <div className="flex items-center gap-3 text-center">
-         <Button 
-         variant="outline" 
-         className="w-8 h-8 rounded-xl"
-         onClick={handleDecreaseQuantity}
-         ><ChevronLeftIcon/></Button>
-         <p className="w-3">{quantity}</p>
-         <Button 
-         variant="destructive" 
-         className="w-8 h-8 rounded-xl"
-         onClick={handleIncreaseQuantity}
-         ><ChevronRightIcon/></Button>
-        </div>
-    </div>
-
-<ScrollArea className="h-full">
-     {/* DESCRIÇÃO */}
-     <div className="mt-6 space-y-3">
-     <h4 className="font-semibold">Sobre</h4>
-     <p className="text-sm text-muted-foreground">{product.description}</p>
+  <ScrollArea className="h-full">
+    {/* DESCRIÇÃO */}
+    <div className="mt-6 space-y-3">
+      <h4 className="font-semibold">Sobre</h4>
+      <p className="text-sm text-muted-foreground">{product.description}</p>
     </div>
 
     {/* INGREDIENTES */}
     <div className="mt-6 space-y-3">
-     <div className="flex items-center gap-1">
-      <ChefHatIcon size={18} />
-     <h4 className="font-semibold">Ingredientes</h4>
-     </div>
-  <ul className="list-disc px-5 text-sm text-muted-foreground">
-   {product.ingredients.map((ingredient) => (
-    <li key={ingredient}>{ingredient}</li>
-    
-   ))}
+      <div className="flex items-center gap-1">
+        <ChefHatIcon size={18} />
+        <h4 className="font-semibold">Ingredientes</h4>
+      </div>
+      <ul className="list-disc px-5 text-sm text-muted-foreground">
+        {product.ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
 
-      </ul> 
+        ))}
+
+      </ul>
     </div>
-</ScrollArea>
+  </ScrollArea>
 
-   </div>
+</div>
 
-    <Button className="rounded-full mt-6 w-full">Adicionar à sacola</Button>
-   </div>
-   );
+<Button className="rounded-full mt-6 w-full" onClick={handleAddToCard}>Adicionar à sacola</Button>
+</div>
+
+<CardSheet/>
+
+    </>
+  );
 }
- 
+
 export default ProductDetails;
--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -12,38 +12,38 @@ interface ProductHeaderProps {
 }
 
 
-const ProductHeader = ({product}: ProductHeaderProps)  => {
+const ProductHeader = ({ product }: ProductHeaderProps) => {
   const router = useRouter();
   const handleBackClick = () => router.back();
- return ( 
- <div className="relative w-full min-h-[300px]">
-  <Button
-         variant="secondary"
-         size="icon"
-         className="absolute left-4 top-4 z-50 rounded-full"
-         onClick={handleBackClick}
-       >
-         <ChevronLeftIcon />
-       </Button>
-       
-       <Image 
-   src={product.imageUrl}
-   alt={product.name}
-   layout="fill"
-   className="object-contain"
-   />
- 
-       <Button
-         variant="secondary"
-         size="icon"
-         className="absolute right-4 top-4 z-50 rounded-full"
-       >
-         <ScrollTextIcon />
-       </Button>
- 
-       
-   
-  </div> );
+  return (
+    <div className="relative w-full min-h-[300px]">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        layout="fill"
+        className="object-contain"
+      />
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+
+
+
+    </div>);
 }
- 
+
 export default ProductHeader;
